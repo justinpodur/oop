b6ef998f833d70d8118e2dacc35619a2ec41ae5a
git-justinpodur <git-justin@podur.org>
2014-06-05 15:43:33 -0400

    created item class

--- salestaxes.rb
+++ salestaxes.rb
@@ -1,15 +1,22 @@
 class Item
-	attr_accessor :price, :salestaxrate, :aftertax
+	attr_accessor :price, :salestaxrate, :importduty, :aftertaxprice
 	attr_reader :name
 
-	def initialize(name,price,salestaxrate,aftertaxprice)
+	def initialize(name,price,salestaxrate,importduty)
 		@name=name
 		@price=price
 		@salestaxrate=salestaxrate
-		@aftertaxprice=aftertaxprice
+		@importduty = importduty
 	end
 
-
-
-
+	def greet
+		puts "I'm a #{name}, I cost #{price}.to_s, which is #{price*(1+salestaxrate+importduty)}.to_s after taxes"
+	end
 end
+
+class Basket
+	attr_accessor :totaltaxes, :totalprice
+end
+
+book=Item.new("book",12.49,0.1,0)
+book.greet
