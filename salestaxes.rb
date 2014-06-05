cdaeea53228f5242c682a01cc4e68c4f9e5b56b1
git-justinpodur <git-justin@podur.org>
2014-06-05 16:08:49 -0400

    got taxcalc working

--- salestaxes.rb
+++ salestaxes.rb
@@ -15,8 +15,38 @@ class Item
 end
 
 class Basket
-	attr_accessor :totaltaxes, :totalprice
+	attr_accessor :itemlist,:totaltaxes, :totalprice
+
+	def initialize(itemlist)
+		@itemlist=itemlist
+		@totaltaxes=0
+		@totalprice=0
+	end
+
+
+	def taxcalc
+		@itemlist.each do |item|
+			puts "loopin"
+			@totaltaxes+=item.price*(item.salestaxrate+item.importduty)
+			@totalprice+=item.price
+		end
+	end
+
+	def receipt
+		taxcalc
+		puts "Your sales total is #{@totalprice} and you've paid #{@totaltaxes}. Remember taxes pay for services."
+	end
 end
 
 book=Item.new("book",12.49,0.1,0)
+#book = ExemptItem.new("book",12.49,1)
 book.greet
+
+cd=Item.new("music CD",16.49,0.1,0)
+
+itemlist=[book,cd]
+puts itemlist[0].inspect
+puts itemlist[1].inspect
+
+shop=Basket.new(itemlist)
+shop.receipt
