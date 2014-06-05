1d8f392ec4ca32e3f10dce4906b072b749a6f841
git-justinpodur <git-justin@podur.org>
2014-06-05 16:38:23 -0400

    more or less completed the assignment, with some bugs

--- salestaxes.rb
+++ salestaxes.rb
@@ -9,6 +9,10 @@ class Item
 		@importduty = importduty
 	end
 
+	def applyduty
+		@price=price*(1+importduty).round(2)
+	end
+
 	def greet
 		puts "I'm a #{name}, I cost #{price.round(2)}, which is #{price*(1+salestaxrate+importduty).round(2)} after taxes"
 	end
@@ -26,7 +30,7 @@ class Basket
 
 	def taxcalc
 		@itemlist.each do |item|
-			@totaltaxes+=item.price*(item.salestaxrate+item.importduty)
+			@totaltaxes+=item.price*(item.salestaxrate)
 			@totalprice+=item.price
 		end
 	end
@@ -41,19 +45,33 @@ class Basket
 	end
 end
 
-book=Item.new("book",12.49,0.1,0)
+book=Item.new("book",12.49,0,0)
 #book = ExemptItem.new("book",12.49,1)
 #book.greet
 
 cd=Item.new("music CD",16.49,0.1,0)
 #cd.greet
 
-chocolate=Item.new("chocolate bar",0.85,0.1,0)
+chocolate=Item.new("chocolate bar",0.85,0,0)
 #chocolate.greet
 
-importchoc=Item.new("imported box of chocolates",10.00,0.1,0.05)
+importchoc=Item.new("imported box of chocolates",10.00,0,0.05)
+importchoc.applyduty
 
-importedperfume=Item.new("imported box of chocolates",47.50,0.1,0.05)
+importedperfume=Item.new("imported bottle of perfume",47.50,0.1,0.05)
+importedperfume.applyduty
+
+importedperfume2=Item.new("imported bottle of perfume",27.99,0.1,0.05)
+importedperfume.applyduty
+
+headachepills=Item.new("packet of headache pills",9.75,0,0)
+headachepills.applyduty
+
+perfume=Item.new("bottle of perfume", 18.99, 0.1, 0)
+perfume.applyduty
+
+importedchoc2=Item.new("box of imported chocolates", 11.25,0,0.05)
+importedchoc2.applyduty
 
 itemlist=[book,cd,chocolate]
 #puts itemlist[0].inspect
@@ -68,3 +86,10 @@ shop2=Basket.new([importchoc,importedperfume])
 puts "Output 2:"
 shop2.receipt
 puts
+
+shop3=Basket.new([importedperfume2,perfume,headachepills,importedchoc2])
+puts "Output 3:"
+shop3.receipt
+puts
+
+#A few small errors... ran out of brainpower
